f54f309dce18aa070c693793f3e1b6d4b2448cf8
Atik471 <atikurr.contact@gmail.com>
2025-08-24 23:37:29 +0600

    fixed import bug in main file

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router";
 import router from "./routers/Router";
-import { HelmetProvider } from "react-helmet-async";
+// import { HelmetProvider } from "react-helmet-async";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./services/queryClient";
 import { AuthProvider } from "./contexts/AuthProvider";
@@ -12,9 +12,7 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AuthProvider>
       <QueryClientProvider client={queryClient}>
-        <HelmetProvider>
-          <RouterProvider router={router} />
-        </HelmetProvider>
+        <RouterProvider router={router} />
       </QueryClientProvider>
     </AuthProvider>
   </StrictMode>
